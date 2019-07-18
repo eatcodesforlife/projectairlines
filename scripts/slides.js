c94f401ddae88fac8504bd226117c8ea1e562024
Cris Acosta <iamcris87@gmail.com>
2019-07-18 14:27:02 +0800

    finished slides.js by brute force

--- scripts/slides.js
+++ scripts/slides.js
@@ -1,33 +1,35 @@
 let businessClassSlide = document.querySelectorAll(".business-class-slide"),
   firstClassSlide = document.querySelectorAll(".first-class-slide"),
-  arrowLeftBusinessClass = document.querySelector(
-    "#prev-button-business-class"
-  ),
-  arrowRightBusinessClass = document.querySelector(
-    "#next-button-bussiness-class"
-  ),
+  businessClassLeftArrow = document.querySelector("#prev-business-class"),
+  businessClassRightArrow = document.querySelector("#next-bussiness-class"),
+  firstClassLeftArrow = document.querySelector("#first-class-prev"),
+  firstClassRightArrow = document.querySelector("#first-class-next"),
   current = 0;
 
-//   reset: clear all images
-function clearImages() {
+// reset: clear images
+function clearImageBusinessClass() {
   businessClassSlide.forEach(img => (img.style.display = "none"));
+}
+function clearImageFirstClass() {
   firstClassSlide.forEach(img => (img.style.display = "none"));
 }
 
-// display first image or initialize
-function startSlide() {
-  clearImages();
+// start slide
+function startSlideBusinessClass() {
+  clearImageBusinessClass();
   businessClassSlide[0].style.display = "block";
+}
+function startSlideFirstClass() {
+  clearImageFirstClass();
   firstClassSlide[0].style.display = "block";
 }
 
-// business class buttons left
-arrowLeftBusinessClass.addEventListener("click", previousBusinessClass);
-arrowRightBusinessClass.addEventListener("click", nextBusinessclass);
+// business class arrows
+businessClassLeftArrow.addEventListener("click", businessClassPrev);
+businessClassRightArrow.addEventListener("click", businessClassNext);
 
-function previousBusinessClass(e) {
-  clearImages();
-
+function businessClassPrev(e) {
+  clearImageBusinessClass();
   if (current === 0) {
     current = businessClassSlide.length;
   }
@@ -35,14 +37,36 @@ function previousBusinessClass(e) {
   current--;
 }
 
-function nextBusinessclass(e) {
-  clearImages();
-
+function businessClassNext(e) {
+  clearImageBusinessClass();
   if (current === businessClassSlide.length - 1) {
-    current = 0;
+    current = -1;
   }
   businessClassSlide[current + 1].style.display = "block";
   current++;
 }
 
-startSlide();
+// first class arrows
+firstClassLeftArrow.addEventListener("click", firstClassPrev);
+firstClassRightArrow.addEventListener("click", firstClassNext);
+
+function firstClassPrev(e) {
+  clearImageFirstClass();
+  if (current === 0) {
+    current = firstClassSlide.length;
+  }
+  firstClassSlide[current - 1].style.display = "block";
+  current--;
+}
+
+function firstClassNext(e) {
+  clearImageFirstClass();
+  if (current === firstClassSlide.length - 1) {
+    current = -1;
+  }
+  firstClassSlide[current + 1].style.display = "block";
+  current++;
+}
+
+startSlideBusinessClass();
+startSlideFirstClass();
